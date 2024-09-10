56c87d0e8bf057aa63848df9032e01cc20cebd90
E226752U <floran.martel@etu.univ-nantes.fr>
2024-09-10 20:10:45 +0200

    test

---
+++ test2.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('#form1');
+
+form.addEventListener('submit', (event) => {
+    const form = event.currentTarget;
+    const url = new URL(form.action);
+    const formData = new FormData(form);
+    const searchParams = new URLSearchParams(formData);
+  
+    /** @type {Parameters<fetch>[1]} */
+    const fetchOptions = {
+      method: form.method,
+    };
+  
+    if (form.method.toLowerCase() === 'post') {
+      if (form.enctype === 'multipart/form-data') {
+        fetchOptions.body = formData;
+      } else {
+        fetchOptions.body = searchParams;
+      }
+    } else {
+      url.search = searchParams;
+    }
+  
+    fetch(url, fetchOptions);
+  
+    event.preventDefault();
+
+});
+
+
