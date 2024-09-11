b1b2b450f124ee8e7c6033ba918a4c7207c16d91
E226752U <floran.martel@etu.univ-nantes.fr>
2024-09-11 20:39:06 +0200

    Start styles

---
+++ test/readcsv.js
@@ -0,0 +1,17 @@
+function decodeCSV(csvtext) {
+    let tableau = csvtext.split("\r\n")
+    tableau = tableau.map((ligne) => {return ligne.split(",")})
+
+    // .shift() remouve la 1er valeur de la list et la retourne
+    return {"head": tableau.shift(), "table": tableau}
+}
+
+
+// const fs = require('fs')
+// fs.readFile('CSVTry.csv', (err, inputD) => {
+//     if (err) throw err;
+//     result = decodeCSV(inputD.toString());
+//     const head = result.head
+//     const table = result.table
+//     console.log("resultat : ",head,table)
+// })
