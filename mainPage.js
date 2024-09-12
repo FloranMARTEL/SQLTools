9f19af32afb0852451938e8ab492860b623c7f56
E226752U <floran.martel@etu.univ-nantes.fr>
2024-09-12 19:36:25 +0200

    styles v0.1

--- mainPage.js
+++ mainPage.js
@@ -1,9 +1,23 @@
 
 console.log("start")
 
+function groupBy(list, keyGetter) {
+  const map = new Map();
+  list.forEach((item) => {
+       const key = keyGetter(item);
+       const collection = map.get(key);
+       if (!collection) {
+           map.set(key, [item]);
+       } else {
+           collection.push(item);
+       }
+  });
+  return map;
+}
 
-document.querySelector('#bb').addEventListener('click', (evt) => {
-  let inputfile = document.querySelector("#test")
+
+document.querySelector('#findrelation').addEventListener('click', () => {
+  let inputfile = document.querySelector("#csvFile")
   let fl_files = inputfile.files; // JS FileList object
 
   // use the 1st file from the list
@@ -16,6 +30,34 @@ document.querySelector('#bb').addEventListener('click', (evt) => {
     result = decodeCSV(e.target.result)
     relation = findkey(result.head,result.table)
     console.log(relation)
+    
+    relatioMaped = groupBy(relation, (element) => element.source)
+    console.log("tes",relatioMaped)
+
+    const block = document.querySelector("#block-deroulant")
+
+    const div1 = document.createElement("div")
+    relatioMaped.forEach(relationGroup => {
+      let divtemp = document.createElement("div")
+      relationGroup.forEach((element) =>{
+        let span =  document.createElement("span")
+        let source = ""
+        element.source.forEach((e) =>{
+          console.log(e)
+          source += e
+        })
+        span.innerHTML = source + " → " + element.destination
+        divtemp.appendChild(span) 
+      })
+      div1.appendChild(divtemp) 
+    });
+
+    div1.classList.add("relation-table")
+    block.appendChild(div1)
+
+    block.style.bottom ="-200px"
+
+
   };
 
   // Read the file as text.
